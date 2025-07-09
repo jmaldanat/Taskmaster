38532318302eb15d7831a10ae18f71d39eaae69f
jmaldanat <jmaldanat@unal.edu.co>
2025-07-09 09:06:35 -0500

    Update settings to use dj_database_url for database configuration

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.exists('env.py'):
     import env
 
@@ -77,10 +78,7 @@ WSGI_APPLICATION = 'taskmaster.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
