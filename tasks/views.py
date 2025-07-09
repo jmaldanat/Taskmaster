dd55f528f0ca8516f60d163ff7bfb84874ac0d78
jmaldanat <jmaldanat@unal.edu.co>
2025-07-09 10:44:06 -0500

    Add initial task list view and template for Taskmaster app

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    to_do_tasks = tasks.filter(completed=False).order_by('due_date')
+    done_tasks = tasks.filter(completed=True).order_by('due_date')
+    context = {
+        'to_do_tasks': to_do_tasks,
+        'done_tasks': done_tasks
+    }
+    return render(request, 'tasks/index.html', context)
